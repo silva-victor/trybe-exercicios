22da4d76ccf9cfa65963e3edbe5288300c3c41ea
Victor <victorfsilva26@gmail.com>
2022-07-25 18:38:03 -0300

    Adicionando exercícios 1 e 2

---
+++ modulo1-Fundamentos/bloco4-introducao-a-js-e-logica/dia-4-javascript-objetos-e-funcoes/ex01e02.js
@@ -0,0 +1,13 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log("Boas vindas,", info['personagem'] + "!");
+
+  info.recorrente = "Sim";
+
+  console.log(info);
+
+  
