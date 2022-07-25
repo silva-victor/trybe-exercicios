9b0aaedf5153dbd527e9b51d54d35ea86555e455
Victor <victorfsilva26@gmail.com>
2022-07-24 21:25:45 -0300

    adicionando os exercícios práticos

--- modulo1-Fundamentos/bloco4-introducao-a-js-e-logica/Dia4-Introducao-a-javascript-e-logica-de-programacao/ex02.js
+++ modulo1-Fundamentos/bloco4-introducao-a-js-e-logica/Dia4-Introducao-a-javascript-e-logica-de-programacao/ex02.js
@@ -1,5 +1,30 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+    // Primeiro exercício
+/*    
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let lista of names) {
-    console.log(lista);
+for (index = 0; index < numbers.length; index += 1) {
+    console.log (numbers[index]);
 }
+        Segundo exercício
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+console.log(numbers [0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
+
+
+        Terceiro exercício
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let media = numbers [0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+
+media = media / 10;
+
+if(media > 20) {
+    console.log ("valor maior ou igual a 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}*/
+
+
