b03ffa8c252ac8a53db4035e7076b04fbba63722
Victor <victorfsilva26@gmail.com>
2022-08-01 01:23:24 -0300

    adicionando exercício 2 e 3

--- modulo1-Fundamentos/bloco5-js-dom-eventos-e-webstorage/dia-3-javascript-eventos/main.js
+++ modulo1-Fundamentos/bloco5-js-dom-eventos-e-webstorage/dia-3-javascript-eventos/main.js
@@ -8,17 +8,46 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 1. Copie esse arquivo e edite apenas ele;
 
+
+
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+
+
+
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
+
+
+
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+
+firstLi.addEventListener('click', function()  {
+  firstLi.className= 'tech';
+})
+
+
+
+
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('click', function(){
+  input.style.backgroundColor= 'red';
+});
+
+
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
+
+
+
+
 // 4.1. Que tal redirecionar para seu portfólio?
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
